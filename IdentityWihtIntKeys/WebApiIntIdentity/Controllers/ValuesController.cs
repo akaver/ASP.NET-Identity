c32629bceb31b283c300b2d99c4ad74b24019bfe
Andres Käver <andres@surf.ee>
2015-05-12 23:24:36 +0300

    initial commit for webapi with int keys in identity

---
+++ IdentityWihtIntKeys/WebApiIntIdentity/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DAL.Interfaces;
+
+namespace WebApiApp.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        private readonly IUOW _uow;
+
+        public ValuesController(IUOW uow)
+        {
+            _uow = uow;
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return _uow.Users.All.Select(a=>a.Email);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
